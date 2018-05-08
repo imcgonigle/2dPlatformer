ef374c6b31572912cc96db6b9d6c6ce66efb1ed5
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2018-05-07 22:58:01 -0600

    Made updates to the Quiz Component

--- Assets/Scripts/Player_Move_Prot.cs
+++ Assets/Scripts/Player_Move_Prot.cs
@@ -7,62 +7,75 @@ public class Player_Move_Prot : MonoBehaviour
 
 	public int playerSpeed = 10;
 	public int playerJumpPower = 1250;
-	private float moveX;
-	public bool isGrounded = false;
-	public bool isTiny = false;
 	public float distanceToBottomOfPlayer = 0.78f;
-	
-	// Update is called once per frame
+	public bool canMove = true;
+
+	private float moveX;
+	private bool isGrounded = false;
+
+	private Animator animator;
+	private SpriteRenderer spriteRenderer;
+	private Rigidbody2D rigidBody;
+
+	void Start ()
+	{
+		// Instantiate componenets.
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
 	void Update ()
 	{
-		PlayerMove ();
-		PlayerRaycast ();
+		PlayerMove (); // Move the player
+		PlayerRaycast (); // Check for collisions
 	}
 
 	void PlayerMove ()
 	{
 		// CONTROLS
-		moveX = Input.GetAxis ("Horizontal");
-		if (Input.GetButtonDown ("Jump") && isGrounded) {
-			Jump ();
-		}
-	
-		if (Input.GetKeyDown ("up")) {
-			if (isTiny) {
-				gameObject.transform.localScale += new Vector3 (0.5f, 0.5f);
-			} else {
-				gameObject.transform.localScale += new Vector3 (-0.5f, -0.5f);
+		if (canMove) {
+			moveX = Input.GetAxis ("Horizontal"); // Float from -1 and 1
+
+			if (Input.GetButtonDown ("Jump") && isGrounded) {
+				Jump ();
 			}
 
-			isTiny = !isTiny;
-		}
-		// ANIMATIONS
-		// PLAYER DIRECTION
-		if (moveX != 0) {
-			GetComponent<Animator> ().SetBool ("isRunning", true);
-		} else {
-			GetComponent<Animator> ().SetBool ("isRunning", false);
-		}
+			// ANIMATIONS
+			animator.SetBool ("isRunning", moveX != 0); // Set isRunning to true if the player is moving.
 
-		// Player Direction
-		if (moveX < 0.0f) {
-			GetComponent<SpriteRenderer> ().flipX = true;
-		} else if (moveX > 0.0f) {
-			GetComponent<SpriteRenderer> ().flipX = false;
-		}
+			// Set the players direction
+			if (moveX != 0) {
+				spriteRenderer.flipX = moveX < 0.0f;
+			}
 
-		// PHYSICS
-		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D> ().velocity.y);
+			// PHYSICS
+			rigidBody.velocity = new Vector2 (moveX * playerSpeed, rigidBody.velocity.y);
+		}
 	}
 
+	/**
+	 * If player is on the ground make them jump into
+	 * the air.
+	 **/
 	void Jump ()
 	{
-		isGrounded = false;
-		GetComponent<Rigidbody2D> ().AddForce (Vector2.up * playerJumpPower);
+		isGrounded = false; // Set isGrounded to false so player can't double jump.
+		rigidBody.AddForce (Vector2.up * playerJumpPower);
 	}
 
+
+	/**
+	 * Check to see if the player has run into other gameobjects.
+	 * - BreakableBoxes
+	 * - Enemies
+	 **/
 	void PlayerRaycast ()
 	{
+		/**
+		 * Check to see if player jumped into
+		 * a breakable box. If so, destroy the box.
+		 **/
 		RaycastHit2D rayUp = Physics2D.Raycast (transform.position, Vector2.up);
 		if (rayUp != null && rayUp.collider != null && rayUp.distance < distanceToBottomOfPlayer) {
 			if (rayUp.collider.tag == "breakable") {
@@ -70,6 +83,10 @@ public class Player_Move_Prot : MonoBehaviour
 			}
 		}
 
+		/**
+		 * Check to see if the player landed on an
+		 * enemy. If so, Destroy the emeny.
+		 * */
 		RaycastHit2D rayDown = Physics2D.Raycast (transform.position, Vector2.down);
 		if (rayDown != null && rayDown.collider != null && rayDown.distance < distanceToBottomOfPlayer) {
 			if (rayDown.collider.tag == "enemy") {
@@ -85,5 +102,23 @@ public class Player_Move_Prot : MonoBehaviour
 				isGrounded = true;
 			}
 		}
+	}
+
+	/**
+	 * Disables player controls. Used whenever there's
+	 * dialogue or a quiz open.
+	 */
+	public void DisableMovement ()
+	{
+		canMove = false;
+	}
+
+	/**
+	 * Enables player movement. Used by dialogue
+	 * and quiz manager after their UI closes.
+	 */
+	public void EnableMovement ()
+	{
+		canMove = true;
 	}
 }
---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+	public Animator animator;
+	GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void ToggleQuiz ()
+	{
+		bool isOpen = animator.GetBool ("isOpen");
+		player.GetComponent<Player_Move_Prot> ().canMove = !isOpen;
+		animator.SetBool ("isOpen", !isOpen);
+	}
+}
